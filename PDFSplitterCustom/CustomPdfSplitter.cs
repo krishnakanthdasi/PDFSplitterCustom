fc7abdcf407f902bb8b01bb603e3a73269e38c05
Karan DotNetDev <karan.netdev@gmail.com>
2023-04-11 21:26:52 +0530

    Add project files.

---
+++ PDFSplitterCustom/CustomPdfSplitter.cs
@@ -0,0 +1,27 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFSplitterCustom
+{
+    public class CustomPdfSplitter : PdfSplitter
+    {
+
+        private String dest;
+        private int partNumber = 1;
+
+        public CustomPdfSplitter(PdfDocument pdfDocument, String dest) : base(pdfDocument)
+        {
+            this.dest = dest;
+        }
+
+        protected override PdfWriter GetNextPdfWriter(PageRange documentPageRange)
+        {
+            return new PdfWriter(String.Format(dest, partNumber++));
+        }
+    }
+}
---
+++ PDFSplitterCustom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using PDFSplitterCustom;
+
+namespace PDFOperations
+{
+    public class PDFOperations
+    {
+        public static readonly String DEST = "../../../Result/Result_{0}.pdf";
+
+        public static readonly String RESOURCE = "../../../10840.pdf";
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new PDFOperations().ManipulatePdf(DEST);
+        }
+
+        public void ManipulatePdf(String dest)
+        {
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(RESOURCE));
+
+            IList<PdfDocument> splitDocuments = new CustomPdfSplitter(pdfDoc, dest).SplitByPageNumbers(new List<int> { 1 ,4, 7, 10 ,40});
+
+            foreach (PdfDocument doc in splitDocuments)
+            {
+                doc.Close();
+            }
+
+            pdfDoc.Close();
+        }
+
+        
+    }
+}
+
+
